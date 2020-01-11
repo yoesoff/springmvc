eb54a38018ddfe5b7a640cbf36638c9038a0f0a0
Mh Yusuf <mhyusufibrahim@gmail.com>
2020-01-11 20:38:29 +0700

    Insert and update form for Product

--- src/main/java/guru/springframework/springmvc/controllers/ProductController.java
+++ src/main/java/guru/springframework/springmvc/controllers/ProductController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import guru.springframework.springmvc.domain.Product;
 import guru.springframework.springmvc.services.ProductService;
 
 /**
@@ -28,8 +30,28 @@ public class ProductController {
     }
 
     @RequestMapping("/products/{id}")
-    public String listProducts(@PathVariable Integer id , Model model) {
+    public String showProduct(@PathVariable Integer id , Model model) {
         model.addAttribute("product", productService.getProductById(id));
         return "product";
     }
+
+    @RequestMapping("/products/{id}/edit")
+    public String editProduct(@PathVariable Integer id , Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "productForm";
+    }
+
+    @RequestMapping("/products/new")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "productForm";
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST )
+    public String saveProduct(Product product) {
+        Product savedProduct = this.productService.saveOrUpdate(product);
+
+        return "redirect:/products/" + savedProduct.getId();
+    }
+
 }
--- src/main/java/guru/springframework/springmvc/services/ProductServiceImpl.java
+++ src/main/java/guru/springframework/springmvc/services/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.springmvc.services;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,7 +17,7 @@ import guru.springframework.springmvc.domain.Product;
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    private Map<Integer, Product> product;
+    private Map<Integer, Product> products;
 
     public ProductServiceImpl() {
         loadProducts();
@@ -24,28 +25,46 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<Product> listAllProducts() {
-        return new ArrayList<>(product.values());
+        return new ArrayList<>(products.values());
     }
-
 
     @Override
     public Product getProductById(Integer id) {
-        return product.get(id);
+        return products.get(id);
     }
 
+    @Override
+    public Product saveOrUpdate(Product product) {
+        if (product!=null) {
+            if (product.getId() == null){
+                product.setId(getNextKey());
+            }
+            
+            this.products.put(product.getId(), product);
+            return product;
+        }
+        
+        throw new RuntimeException("Error kawan!");
+        
+    }
+
+    private Integer getNextKey(){
+        return Collections.max(products.keySet()) + 1;
+    }      
+    
     public void loadProducts() {
-        product = new HashMap<>();
+        products = new HashMap<>();
 
         Product product1 = new Product(1, "Product 1 lorem ipsum", new BigDecimal("22"), "lorem");
-        product.put(1, product1);
+        products.put(1, product1);
 
         Product product2 = new Product(2, "Product 2 lorem ipsum", new BigDecimal("23"), "http://example.com/product2");
-        product.put(2, product2);
+        products.put(2, product2);
 
         Product product3 = new Product(3, "Product 3 lorem ipsum", new BigDecimal("25"), "http://example.com/product3");
-        product.put(3, product3);
+        products.put(3, product3);
 
         Product product4 = new Product(4, "Product 4 lorem ipsum", new BigDecimal("40"), "http://example.com/product4");
-        product.put(4, product4);
+        products.put(4, product4);
     }
 }
